04d0d011e0774bbfd517d6a9ef87fb334d9e3a8e
MARI <mari20201101@gmail.com>
2021-09-18 00:11:50 +0900

    Receive command line args

---
+++ src/array/EnterCommand.java
@@ -0,0 +1,10 @@
+package array;
+
+public class EnterCommand {
+    public static void main(String[] args) {
+        System.out.println("args length : " + args.length);
+        for(String s : args){
+            System.out.println("args .. "  + s);
+        }
+    }
+}
